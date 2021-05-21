24c96b0f081e14e07e6bdd405078f89c242f7920
Devapritam Sharma <dsarma1980@gmail.com>
2021-05-20 23:50:46 -1200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function addUser() {
+    player1 = document.getElementById("user1_input").value;
+    player2 = document.getElementById("user2_input").value;
+
+    localStorage.setItem("player1", player1);
+    localStorage.setItem("player2", player2);
+
+    window.location = "game_play.html";
+}
